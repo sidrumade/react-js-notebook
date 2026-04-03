33df05ef39e910b571e6df14054ed3cff5fc610d
siddhesh rumade <siddhesh.rumade.5@gmail.com>
2026-04-03 22:38:10 +0530

    notebook version updated

--- src/Components/HelpComponent.js
+++ src/Components/HelpComponent.js
@@ -4,33 +4,33 @@ import '../help.css';
 
 const HelpComponent = () => {
     return (<div className="helpcomponent">
-    <h4>Server Information:</h4>
-    <p>You are using Javascript notebook.<br></br>
-    The version of the notebook server is: <b>1.2.0</b>
-    <br></br>
-    <br></br>
-    <p className = "title">Shortcut1 (Run Current Cell)</p>
-    <pre>Ctrl + Enter</pre>
+        <h4>Server Information:</h4>
+        <p>You are using Javascript notebook.<br></br>
+            The version of the notebook server is: <b>2.0</b>
+            <br></br>
+            <br></br>
+            <p className="title">Shortcut1 (Run Current Cell)</p>
+            <pre>Ctrl + Enter</pre>
 
-    <p className = "title">Shortcut2 (Run Current Cell and go to next Cell)</p>
-    <pre>Shift + Enter</pre>
+            <p className="title">Shortcut2 (Run Current Cell and go to next Cell)</p>
+            <pre>Shift + Enter</pre>
 
-    <p className = "title">Display Variable</p>
-    <pre>show(variable_name);</pre>
-    
-    <p className = "title"> Load External Library</p>
-    <pre>loadLibrary('lib url');</pre>
+            <p className="title">Display Variable</p>
+            <pre>show(variable_name);</pre>
 
-    <p className = "title">Insert Html Element</p>
-    <pre>insertHTML('html code');</pre>
-    
-    <p className = "title">Execute DOM/UI Script on Main Thread</p>
-    <pre>{"displayOnMainThread('<div id=\"box\"></div>', function(arg) { /* DOM Logic */ }, [arg]);"}</pre>
+            <p className="title"> Load External Library</p>
+            <pre>loadLibrary('lib url');</pre>
+
+            <p className="title">Insert Html Element</p>
+            <pre>insertHTML('html code');</pre>
+
+            <p className="title">Execute DOM/UI Script on Main Thread</p>
+            <pre>{"displayOnMainThread('<div id=\"box\"></div>', function(arg) { /* DOM Logic */ }, [arg]);"}</pre>
 
 
-    </p>
+        </p>
     </div>);
-        
+
 }
 
-            export default HelpComponent;
+export default HelpComponent;
